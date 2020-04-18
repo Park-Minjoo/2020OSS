ce0685a498bac7e7eed1cf72ace1fb99b8e39ffb
Park Minjoo <21800268@handong.edu>
2020-04-19 03:30:19 +0900

    Add searchNum to manager.h

--- manager.h
+++ manager.h
@@ -4,6 +4,7 @@
 
 void listProduct(Product *plist, int count); //전체 제품의 리스트를 출력
 int selectDataNo(Product *p, int count); //제품의 번호를 선택하는 함수
-void saveData(Product *p, int count); //등록된 제품리스트를 파일에 저장
+
 void saveData(Product *p, int count); //등록된 제품리스트를 파일에 저장
 int loadData(Product *p); // 파일을 불러오는 함수
+void searchName(Product *p, int count); //파일속의 이름을 찾는 함수
